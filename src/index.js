1685ca0b6c6e3ebf99aa1c8ecc8c0bfcf066c945
ahmadhamzh <ahmadhamzh@ymail.com>
2021-09-28 13:52:21 +0300

    add auth0 in .env

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
+require('dotenv').config();
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
   <Auth0Provider
-  domain="dev-pkkglc0a.us.auth0.com"
-  clientId="4s0A1IvBWMrPBmOUcgngAaF3SucJgC44"
+  domain={process.env.REACT_APP_AUTH0_DOMAIN}
+  clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
   redirectUri={window.location.origin}
 >
   <React.StrictMode>
